ad20d17589af216148a2e8b728bfb964d5b27202
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-09 13:39:59 +0530

    tactic 3: ask model to check whether conditions are satisfied

--- prompt-engineering/lesson-1-guidelines.py
+++ prompt-engineering/lesson-1-guidelines.py
@@ -21,24 +21,40 @@ def get_completion(prompt, model="gpt-4o-mini"):
 
     return response.choices[0].message.content
 text = f"""
-Alfred is a BCA Student at Gu university. \
-He is 21 years old.\
-He likes watching football matches \
-He doesn't like indian food \
+Making tea is easy, \
+Boil water, place a tea bag in a cup,\
+Pour hot water over the tea bag .\
+let the tea steep \
+Remove the bad and add sugar or milk if desired \
 
 
 """
+text_2 = f"""
+The sun is the center of our solar system. \
+It contains most of the solar system's mass. \
+"""
+
+
 user_input = input("Ask anything: ")
 prompt = f"""
 You must answer using ONLY the information \
 inside the triple backticks. \
+
 If the answer cannot be found in the context, \
 say "I don't have enough information." \
+
+If the context contains steps, write step by step in, \
+step 1: 
+step 2:
+step n:
+      .\
+if the there is no steps, \
+write "No steps provided." \
 
 User Question:
 {user_input}
 
-```{text}```
+```{text_2}```
 
 Do not say "Based on the information provided". \
 
