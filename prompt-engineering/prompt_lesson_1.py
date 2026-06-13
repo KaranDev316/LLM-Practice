359945e343debefe8ebab90f392003bcffbd84f5
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-13 14:57:47 +0530

    identifying emition

--- prompt-engineering/prompt_lesson_1.py
+++ prompt-engineering/prompt_lesson_1.py
@@ -7,8 +7,10 @@ def build_prompt(user_input):
     
     
     
-    Give your answer as a single word, either "positive" \
-    or "negative".
+    Identify a list of emotions that the writer of the \
+    following review is expressing. Include no more than \
+    five items in the list. Format your answer as a list of \
+    lower-case words separated by commas.
     
     user message:
     {user_input}
